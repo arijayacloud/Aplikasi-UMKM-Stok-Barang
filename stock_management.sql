6b2dbf1e2c35c13286187a35225230270676d530
Rizqyalfy <148181672+Rizqyalfy@users.noreply.github.com>
2026-03-02 09:38:52 +0700

    Add files via upload

--- stock_management.sql
+++ stock_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 02, 2026 at 02:32 AM
+-- Generation Time: Mar 02, 2026 at 02:38 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -98,13 +98,6 @@ CREATE TABLE `outlets` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `outlets`
---
-
-INSERT INTO `outlets` (`id`, `name`, `address`, `created_at`) VALUES
-('1772416853494', 'test', 'test', '2026-03-02 02:00:53');
-
 -- --------------------------------------------------------
 
 --
@@ -116,13 +109,6 @@ CREATE TABLE `outlet_stock` (
   `product_id` varchar(255) NOT NULL,
   `quantity` int DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `outlet_stock`
---
-
-INSERT INTO `outlet_stock` (`outlet_id`, `product_id`, `quantity`) VALUES
-('1772416853494', '1772415525032', 50);
 
 -- --------------------------------------------------------
 
@@ -152,13 +138,6 @@ CREATE TABLE `products` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `name`, `category`, `stockPusat`, `price`, `created_at`) VALUES
-('1772415525032', 'Ram', 'Storage', 100, 500000, '2026-03-02 01:38:45');
-
 -- --------------------------------------------------------
 
 --
@@ -178,6 +157,14 @@ CREATE TABLE `users` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `consignments`
+--
+ALTER TABLE `consignments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `outlet_id` (`outlet_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `expenses`
@@ -247,6 +234,13 @@ ALTER TABLE `invoice_items`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `consignments`
+--
+ALTER TABLE `consignments`
+  ADD CONSTRAINT `consignments_ibfk_1` FOREIGN KEY (`outlet_id`) REFERENCES `outlets` (`id`),
+  ADD CONSTRAINT `consignments_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 --
 -- Constraints for table `expenses`
